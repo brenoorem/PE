cc038d4e74c69c61ffd0916a49d06feba9528cd5
Breno Rosa Orem <breno.orem@gmail.com>
2024-09-24 15:36:57 -0300

    Adição de Exercícios

--- Atividades 24-09-03/alocacao_dinamica.c
+++ Atividades 24-09-03/alocacao_dinamica.c
@@ -2,20 +2,72 @@
 #include <stdlib.h>
 
 void aloca(float **ptr, int tam);
+void leitura(float *ptr);
+void mostra(float *p, int cont);
+void media(float *p, int tam, float **pM, int *qM);
 
 int main()
 {
-    float *ptr = NULL;
-
-    aloca(&ptr, 10);
-
-}
-
-void aloca(float **p, int tam)
-{
-    if((*p=(float*)realloc(*p, tam*sizeof(float))) == NULL)
+    float *ptr = NULL, *ptrMaior=NULL;
+    int cont=0, qMaior=0;
+    char op;
+    
+    do
     {
-        printf("Erro na Alocação");
+        aloca(&ptr,cont+1);
+        leitura(ptr+cont);
+        cont++;
+        printf("\ndeseja continuar? (s/n)\n");
+        scanf(" %c",&op);
+    }
+    while (op != 'n' && op != 'N');
+
+    printf("\nnumeros digitados:\n");
+    mostra(ptr,cont);
+    media(ptr,cont,&ptrMaior,&qMaior);
+    printf("\nnumeros maiores que media:\n");
+    mostra(ptrMaior,qMaior);
+}//main
+
+void aloca (float **ptr, int tam)
+{
+    if((*ptr=(float*)realloc(*ptr,tam*sizeof(int)))==NULL)
+    {
+        printf("erro no programa!");
         exit(1);
     }
+}//aloca
+
+void leitura(float *p)
+{
+    printf("\ndigite um numero: ");
+    scanf("%f", p);
+    fflush(stdin);
+}//leitura
+
+void mostra(float *p, int tam)
+{
+    int i;
+    for(i=0;i<tam;i++,p++)
+        printf("%.2f\n",*p);
+}
+
+void media(float *p, int tam, float **pM, int *qM)
+{
+    int i;
+    float m;
+
+    for(i=0;i<tam;i++)
+        m+=*(p+i);
+
+    m/=tam;
+    printf("\na media dos numeros e: %.2f\n",m);
+
+    for(i=0;i<tam;i++)
+        if(*(p+i)>m)
+        {
+            aloca(pM,*qM);
+            *(*pM+*qM) = *(p+i);
+            (*qM)++;
+        }
 }
