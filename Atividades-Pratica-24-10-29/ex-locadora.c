b712c22be62c6b8d9d1c18d43f0544a72b3954eb
Breno Orem <brenoorem@MacBook-Air-de-Breno.local>
2024-10-29 20:24:18 -0300

    Correção de Exercícios

--- Atividades-Pratica-24-10-29/ex-locadora.c
+++ Atividades-Pratica-24-10-29/ex-locadora.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h> 
 
 typedef struct carro{
     int reg_carro;
@@ -20,9 +21,11 @@ void aloca_cliente(cliente **p, int tam);
 void cadastra_carro(carro *p, int tam);
 void cadastra_cliente(cliente *p, carro *pcar);
 void mostra_carro(carro *p, int tam);
+void mostra_cliente(cliente *p, int tam);
 int busca_carro(carro *p, int tam, char tipo_car);
 int busca_cliente(cliente *p, int tam, char *vCPF);
 void devolucao(cliente *p, carro *pcar, int tam);
+
 int main()
 {
     int op;
@@ -36,7 +39,7 @@ int main()
     do
     {
         system("cls");
-        printf("Locadora de Veiculos\n1 - Locacao\n2 - Devolucao\n\n");
+        printf("Locadora de Veiculos\n\n[1] - Locacao\n[2] - Devolucao\n[3] - Mostra Carros\n[4] - Mostra Clientes\n[5] - Sair\n\n");
         scanf("%i", &op);
         fflush(stdin);
         switch(op)
@@ -52,9 +55,16 @@ int main()
             case 2:
                 devolucao(cli,car,cont);
                 break;
+            
+            case 3:
+                mostra_carro(car,15);
+                break;
 
+            case 4:
+                mostra_cliente(cli,cont);
+                break;
         }
-    } while (op != 3);
+    } while (op != 5);
     
 }
 
@@ -105,7 +115,7 @@ void cadastra_cliente(cliente *p, carro *pcar)
     fflush(stdin);
     tipo_car=toupper(tipo_car);
     num_car=busca_carro(pcar,15,tipo_car);
-    num_car=p->num_reg;
+    p->num_reg=num_car;
 
     if(num_car==-1)
         printf("Nao ha carros disponiveis na categoria!\n\n");
@@ -115,7 +125,7 @@ void cadastra_cliente(cliente *p, carro *pcar)
         fgets(p->nome,80,stdin);
         fflush(stdin);
         printf("\nCPF: ");
-        fgets(p->nome,13,stdin);
+        fgets(p->CPF,13,stdin);
         fflush(stdin);
         printf("\nPeriodo do Aluguel (em Dias): ");
         scanf("%i", &(p->dias));
@@ -130,8 +140,8 @@ int busca_carro(carro *p, int tam, char tipo_car)
     {
         if(p->status == 'L' && p->tipo == tipo_car)
         {
-            return p->reg_carro;
             p->status='A';
+            return p->reg_carro;
         }
     }
     return -1;
@@ -168,7 +178,7 @@ void devolucao(cliente *p, carro *pcar, int tam)
         tip = (pcar+reg-1)->tipo;
         tempo = (p+pos)->dias;
 
-        printf("Tipo do carro: %i\n", tip);
+        printf("\nTipo do carro: %c\n", tip);
         switch(tip)
         {
             case 'P': printf("\nValor a pagar: %.2f\n\n", tempo*150.00);
@@ -185,18 +195,32 @@ void devolucao(cliente *p, carro *pcar, int tam)
 int busca_cliente(cliente *p, int tam, char *vCPF)
 {
     int i;
+    printf("\n%s", vCPF);
+    printf("\n%s", p->CPF);
     
-    for(i=0;i<tam;i++,p++)
+    for(i=0;i<tam;i++)
     {
-        if(strcmp(p->CPF, vCPF)==0)
+        if(strcmp(vCPF,(p+i)->CPF)==0)
             {
-                printf("Nome: %c", p->nome);
-                printf("Registro do Carro: %c", p->num_reg);
-                printf("Tempo de Aluguel: %c", p->dias);
+                printf("\n\nCliente: %s", (p+i)->nome);
+                printf("Registro do Carro: %i", (p+i)->num_reg);
+                printf("\nTempo de Aluguel: %i dias", (p+i)->dias);
 
                 return i;
             }
     }
 
     return -1;
+}
+
+void mostra_cliente(cliente *p, int tam)
+{
+    int i;
+    for(i=0;i<tam;i++,p++)
+    {
+        printf("\nNome: %s", p->nome);
+        printf("CPF: %s", p->CPF);
+        printf("\nTempo de Aluguel: %i", p->dias);
+        printf("\nCarro Alugado: %i\n", p->num_reg);
+    }
 }
